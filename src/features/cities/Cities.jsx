c333f321c6aee59424ff653ebcd3116b785f4a27
wimkinnet <kinnet.ceulemans@gmail.com>
2025-02-04 16:56:55 +0100

    Added city details

--- src/features/cities/Cities.jsx
+++ src/features/cities/Cities.jsx
@@ -8,6 +8,8 @@ export default function Cities() {
 
     const [city, setCity] = useState("")
     const [results, setResults] = useState([])
+    const [selectedCity, setSelectedCity] = useState(null)
+    const [selectedCityIsEmpty, setSelectedCityIsEmpty] = useState(true)
     const dispatch = useDispatch()
     const cities = useSelector(citiesList)
 
@@ -15,32 +17,39 @@ export default function Cities() {
         const endpoint = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=5&format=json`;
         fetch(endpoint).then(response => response.json()).then(data => data.results).then(data => data ? setResults(data.filter(city => city.population > 1000)): setResults(data))
     }, [city])
-
-    const cityItems = Object.values(cities).map(city => <li id={city.id}>{city.name} | {city.country}</li>)
     
     const handleCityChange = (e) => {
         setCity(e.target.value)
     }
-
+    
     const handleCityResultsClick = (e) => {
         
-        const selectedId = e.target.getAttribute('id')
-        const selectedCity = results.findIndex((city) => city.id == selectedId)
+        const Id = e.target.getAttribute('id')
+        const City = results.findIndex((city) => city.id == Id)
 
         dispatch(addCity({
-            id: results[selectedCity].id,
-            name: results[selectedCity].name,
-            country: results[selectedCity].country,
-            longitude: results[selectedCity].longitude,
-            latitude: results[selectedCity].latitude,
-            elevation: results[selectedCity].elevation,
-            population: results[selectedCity].population
+            id: results[City].id,
+            name: results[City].name,
+            country: results[City].country,
+            longitude: results[City].longitude,
+            latitude: results[City].latitude,
+            elevation: results[City].elevation,
+            population: results[City].population
         }))
        
         setCity("")
     }
 
+    const handleCityListClick = (e) => {
+        
+        const id = e.target.getAttribute('id')
+        setSelectedCity(id)
+        cities[selectedCity] ? setSelectedCityIsEmpty(false) : setSelectedCityIsEmpty(true)
+    }
+
     const listItems = results ? results.map(city => <li id={city.id} onClick={handleCityResultsClick}>{city.name} | {city.country}</li>) : null
+
+    const cityItems = Object.values(cities).map(city => <li id={city.id} onClick={handleCityListClick}>{city.name} | {city.country}</li>)
 
     return (
         <>
@@ -51,6 +60,13 @@ export default function Cities() {
             <ul>{listItems}</ul>
             {cityItems.length > 0 && <h3>Click a city to see details</h3>}
             <ul>{cityItems}</ul>
+            {!selectedCityIsEmpty && (
+                <ul>
+                    <li>name: {cities[selectedCity].name}</li>
+                    <li>country: {cities[selectedCity].country}</li>
+                    <li>longitude: {cities[selectedCity].longitude}</li>
+                </ul>
+            )}
         </>
     )
 }
